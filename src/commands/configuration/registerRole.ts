be01a9ea7e58db48903cda46fa0a050096eab6c9
CTN <info@ctn-originals.com>
2024-11-20 15:32:05 +0100

    finished /register-role options

--- src/commands/configuration/registerRole.ts
+++ src/commands/configuration/registerRole.ts
@@ -1,93 +1,38 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ChannelSelectMenuBuilder, ChannelSelectMenuInteraction, ChannelType, CommandInteraction, EmbedBuilder, MentionableSelectMenuBuilder, MentionableSelectMenuInteraction, RoleSelectMenuBuilder, RoleSelectMenuInteraction, SlashCommandBuilder } from "discord.js";
-import { AnyComponentInteraction } from "../../@types/discord";
+import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 
 import generalData from '../../data'
+import { PeriodOfTime } from "../../utils";
 
 export default {
 	command: {
 		data: new SlashCommandBuilder()
 			.setName("register-role")
 			.setDescription("Register a new mention role and configure its behaviour")
-			.addMentionableOption(option => option
+			.addRoleOption(option => option
 				.setName('role')
 				.setDescription('The role to be registered')
 				.setRequired(true)
 			)
 			.addStringOption(option => option
 				.setName('cooldown')
-				.setDescription('The cooldown to apply to the mention once its used. (8s 28h 1d = 8 seconds, 4 hours and 2 days)')
+				.setDescription('The cooldown to apply to the mention once its used (seperate with spaces). 8s 28h 1d = 2d 04:00:08')
 				.setRequired(true)
 				.setMinLength(2)
 			),
 		async execute(interaction: CommandInteraction) {
-			const allowedRoleSelect = new RoleSelectMenuBuilder({
-				custom_id: "register-role_allowed-role",
-				min_values: 0,
-				max_values: 25,
-				placeholder: "Role(s) allowed to use (0 for all)"
-			});
-			const channelSelect = new ChannelSelectMenuBuilder({
-				custom_id: "register-role_channel",
-				min_values: 0,
-				max_values: 25,
-				placeholder: "Channels to allow (0 for all)",
-				channelTypes: [ChannelType.GuildText, ChannelType.GuildForum, ChannelType.PrivateThread, ChannelType.PublicThread]
-			})
-			const submitButton = new ButtonBuilder({
-				custom_id: "register-role_submit",
-				label: "Submit",
-				style: ButtonStyle.Success
-			})
-
-			// const row1: ActionRowBuilder<any> = new ActionRowBuilder().addComponents(roleSelect)
-			const row1: ActionRowBuilder<any> = new ActionRowBuilder().addComponents(allowedRoleSelect)
-			const row2: ActionRowBuilder<any> = new ActionRowBuilder().addComponents(channelSelect)
-			const row3: ActionRowBuilder<any> = new ActionRowBuilder().addComponents(submitButton)
+			const role = interaction.options.get('role', true).value;
+			const cooldown = new PeriodOfTime(interaction.options.get('cooldown', true).value as string)
 
 			await interaction.reply({
 				embeds: [new EmbedBuilder({
-					title: "Register Role Configuration",
-					description: "Please fill out the fields below"
+					title: "Registered New Role Cooldown",
+					fields: [
+						{name: 'role', value: `<@&${role}>`, inline: true},
+						{name: 'cooldown', value: `\`${cooldown.toString()}\``, inline: true},
+					]
 				})],
-				components: [row1, row2, row3],
 				ephemeral: !generalData.development
 			});
 		}
 	},
-	selectMenus: [
-		{
-			data: new SlashCommandBuilder().setName("register-role_allowed-role"),
-			async execute(interaction: MentionableSelectMenuInteraction) { interaction.deferUpdate() }
-		},
-		{
-			data: new SlashCommandBuilder().setName("register-role_channel"),
-			async execute(interaction: ChannelSelectMenuInteraction) { interaction.deferUpdate() }
-		},
-	],
-	buttons: [
-		{
-			data: new SlashCommandBuilder().setName("register-role_submit"),
-			async execute(interaction: ButtonInteraction) { 
-				interaction.reply({
-					embeds: [new EmbedBuilder({
-						title: "Saved Role Configurations",
-						fields: [{name: 'TODO', value: 'Add resulting configuration fields'}]
-					})],
-					ephemeral: !generalData.development
-				})
-			}
-		}
-	]
 }
-// async function executeSelectMenu(interaction: AnyComponentInteraction) {
-// 	await interaction.reply({
-// 		content: "",
-// 		embeds: [new EmbedBuilder({
-// 			fields: [
-// 				{name: "Selected", value: interaction.values.join(' | ') ?? "None"}
-// 			]
-// 		})],
-// 	});
-
-// 	return true;
-// }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -30,4 +30,66 @@ export class PeriodOfTime {
 		//? get the time in miliseconds
 		this.time = duration(this.formatedInput).asMilliseconds();
 	}
+
+	public toString(compact: boolean = true): string {
+		const units = timeUnits(this.time);
+		const formatTime = (t: number) => {return (t < 10) ? `0${t}` : t}
+
+		if (compact) {
+			return `${units.days}d ${formatTime(units.hours)}:${formatTime(units.minutes)}:${formatTime(units.seconds)}`
+		} else {
+			return `${units.days} days ${units.hours} hours ${units.minutes} minutes ${units.seconds} seconds`
+		}
+	}
+
+	public get dateTime(): Date {
+		return new Date(this.time);
+	}
+}
+
+/**
+ * Converts milliseconds into greater time units as possible
+ * 
+ * source: https://stackoverflow.com/a/68673714
+ * @param {int} ms - Amount of time measured in milliseconds
+ * @return {?Object} Reallocated time units. NULL on failure.
+ */
+export function timeUnits(ms: number): {
+	days: number,
+	hours: number,
+	minutes: number,
+	seconds: number,
+	milliseconds: number
+} {
+	if ( !Number.isInteger(ms) ) { 
+		return {
+			days: 0,
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+			milliseconds: 0
+		}
+	}
+
+	/**
+	 * Takes as many whole units from the time pool (ms) as possible
+	 * @param {int} msUnit - Size of a single unit in milliseconds
+	 * @return {int} Number of units taken from the time pool
+	 */
+	const allocate = (msUnit: number) => {
+		const units = Math.trunc(ms / msUnit)
+		ms -= units * msUnit
+		return units
+	}
+
+	// Property order is important here.
+	// These arguments are the respective units in ms.
+	return {
+		// weeks: allocate(604800000), // Uncomment for weeks
+		days: allocate(86400000),
+		hours: allocate(3600000),
+		minutes: allocate(60000),
+		seconds: allocate(1000),
+		milliseconds: ms // remainder
+	}
 }
