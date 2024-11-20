b4decfb870718874e59d24dc9c36711a9cfaf564
CTN <info@ctn-originals.com>
2024-11-20 18:29:34 +0100

    cleaned up the simulation
    Removed the whole simulation dir and just made a simple class in the ready event script that simply works

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
-import { APIUser, Channel, Client, Collection, CommandInteraction, EmbedBuilder, Events, Guild, GuildMember, Message, MessageCollector, REST, Routes, TextChannel, User } from 'discord.js';
-// import { InteractionResponses } from "discord.js/src/structures/interfaces/InteractionResponses";
+import { Client, ComponentType, EmbedBuilder, Events, Guild, Interaction, InteractionType, Message, Routes, TextChannel } from 'discord.js';
 
 import { ConsoleInstance } from 'better-console-utilities';
 
@@ -9,9 +8,7 @@ import { devEnvironment } from '../data';
 import { EmitError, customEvents } from '.';
 import { cons, errorConsole, testWebhook } from '..';
 import { testEmbed, validateEmbed } from '../utils/embedUtils';
-import { SimBaseInteraction, defaultBaseInteractionArgs } from '../simulators';
-import { SimCommandInteraction } from '../simulators/commandInteraction';
-import { SimBaseComponentInteraction, SimStringSelectMenuInteraction } from '../simulators/componentInteraction';
+import { TODO } from '../@types';
 
 // import ErrorHandler from '../handlers/errorHandler';
 
@@ -55,27 +52,90 @@ export default {
 		// 	}
 		// })
 
-		// const simBaseInteraction = new SimBaseInteraction();
-		// const simInteraction = new SimCommandInteraction({
-		// 	commandName: 'ping',
-		// 	...defaultBaseInteractionArgs,
-		// });
-
-		// // console.log(InteractionResponses.prototype)
-		// // cons.logDefault(simBaseInteraction);
-		// // cons.logDefault(simInteraction);
-		// // cons.logDefault('Command Simulation:', simInteraction.simulate());
-		// simInteraction.simulate()
-		// const simReply = await simInteraction.awaitReply();
-		// let simComponentInteraction: SimBaseComponentInteraction;
-		// if (simReply) {
-		// 	try {
-		// 		simComponentInteraction = new SimStringSelectMenuInteraction({customId: 'string-select-test', message: simReply, values: ['hello']});
-		// 		simComponentInteraction.simulate()
-		// 		cons.logDefault(simComponentInteraction.component)
-		// 	} catch (err) {
-		// 		errorConsole.log(`Error while trying to create SimBaseComponentInteraction`)
-		// 	}
-		// }
+		new FakeInteraction('rolecooldown', {
+			subCommand: 'add',
+			options: [
+				{name: 'role', value: '811667577998671923'},
+				{name: 'cooldown', value: '80s 3d 34h 400m awd23a'}
+			]
+		}).execute();
 	}
 };
+
+type InteractionOptionEntry = {name: string, value: any}
+type FakeInteractionInput = {
+	subCommand?: string,
+	options: InteractionOptionEntry[]
+}
+
+class FakeInteractionOptions {
+	constructor(
+		public _hoistedOptions: InteractionOptionEntry[] = [],
+		public subCommand?: string,
+		public group?: string
+	) {}
+
+	public get(option: string) {
+		return this._hoistedOptions.find(o => o.name === option)
+	}
+	//TODO add typed getters like getString or getRole...
+
+	public getSubcommand(required: boolean) { return this.subCommand }
+	public getSubcommandGroup(required: boolean) { return this.group }
+
+	public get _subcommand() { return this.subCommand }
+
+	public get data() { //TODO include groups
+		if (!this.subCommand) { return this._hoistedOptions }
+		else {
+			return [{
+				name: this.subCommand,
+				options: this._hoistedOptions,
+				type: 1
+			}]
+		}
+	}
+}
+
+class FakeInteraction {
+	public client: Client<boolean> = devEnvironment.client!;
+	public guild: Guild = devEnvironment.guild!;
+	public type: InteractionType = InteractionType.ApplicationCommand;
+	public componentType: ComponentType = 1;
+
+	public user: TODO;
+	public channel: TODO
+
+	public options: FakeInteractionOptions;
+
+	constructor(
+		public commandName: string,
+		options: FakeInteractionInput
+	) {
+		this.user = {
+			id: process.env.DEV_TEST_USER_ID!,
+			username: 'keybotkiller',
+			_equals: (user) => {return true},
+		}
+		this.channel = {
+			id: process.env.DEV_TEST_CHANNEL_ID!,
+			name: 'bot-testing',
+		}
+
+		this.options = new FakeInteractionOptions(options.options, options.subCommand);
+	}
+
+	public get channelId() { return this.channel.id }
+	public get guildId() { return this.guild.id };
+
+	public isRepliable() {return true};
+	public async reply(replyContent: string | {content: string, ephemeral: boolean, embeds: EmbedBuilder[], components: any[]}): Promise<Message|boolean> {
+		const channel = this.guild.channels.cache.get(this.channel.id);
+		if (!channel || !(channel instanceof TextChannel)) return false;
+		return channel.send(replyContent);
+	}
+
+	public execute() {
+		this.client.emit('interactionCreate', this as unknown as Interaction)
+	}
+}
