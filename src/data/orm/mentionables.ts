0013263b8e52c66604c97e7c93213e662b65697d
CTN <info@ctn-originals.com>
2024-11-22 22:34:37 +0100

    Update mentionables.ts
    namespace to static class

--- src/data/orm/mentionables.ts
+++ src/data/orm/mentionables.ts
@@ -3,31 +3,58 @@ import { EmitError } from "../../events";
 import { default as Database, IMentionableItem, IMentionableStorage } from "./schemas/mentionableData";
 import { Guild, Role } from "discord.js";
 import { cons } from "../..";
+import { ObjectRelationalMap } from ".";
 
-export namespace Mentionable {
+export class Mentionable {
 	//? true if the anything has updated sins getAll() was last called
-	let hasChanged: boolean = true;
+	public static hasChanged: boolean = true;
 	
 	//? Store the mentionable object here everytime getAll() is called while hasChanged is true
 	//? This is to save a bit of performance as the getAll function might be called for each message sent in any server.
-	let mentionablesCache: IMentionableStorage = {};
+	public static mentionablesCache: IMentionableStorage = {};
 
 	
 	//#region Getters
+	/** Get the whole document of a guild
+	 * @param guildId The ID of the guild
+	 * @param errorIfNull Should an error be logged if the document doesnt exist?
+	*/
+	public static async getDocument(guildId: string, errorIfNull: boolean = true) {
+		// if (errorOnNull) {
+		// 	try {
+		// 		return Database.findOne({ _id: guildId });
+		// 	} catch (e) {
+		// 		const errMessage = `Attempted to find document of guild (${guildId})\n`;
+		// 		if (e instanceof Error) {
+		// 			e.message = errMessage + e.message
+		// 			EmitError(e);
+		// 		} else {
+		// 			EmitError(new Error(errMessage));
+		// 		}
+		// 		return null;
+		// 	}
+		// }
+		// else {
+		// 	return Database.findOne({ _id: guildId });
+		// }
+
+		return await ObjectRelationalMap.getDocument(Database, guildId, errorIfNull)
+	}
+
 	/** Get a list of all mentionables in a server
 	 * @param guildId The GuildID of the server the mentionable is in
 	 * @returns An object containing all mentionables
 	*/
-	export async function getAll(guildId: string): Promise<IMentionableStorage|null> {
-		if (hasChanged) {
-			const doc = await getDocument(guildId);
+	public static async getAll(guildId: string): Promise<IMentionableStorage|null> {
+		if (Mentionable.hasChanged) {
+			const doc = await Mentionable.getDocument(guildId);
 			if (!doc) { return null; }
 
-			mentionablesCache = doc.mentionables;
-			hasChanged = false;
+			Mentionable.mentionablesCache = doc.mentionables;
+			Mentionable.hasChanged = false;
 		}
 
-		return mentionablesCache;
+		return Mentionable.mentionablesCache;
 	}
 
 	/** Get a mentionable by ID
@@ -35,39 +62,15 @@ export namespace Mentionable {
 	 * @param id The ID of the mentionable
 	 * @returns The mentionable if found, null otherwise
 	*/
-	export async function get(guildId: string, id: string): Promise<IMentionableItem|null> {
-		return await getAll(guildId)[id];
+	public static async get(guildId: string, id: string): Promise<IMentionableItem|null> {
+		return await Mentionable.getAll(guildId)[id];
 	}
 	
-	/** Get the whole document of a guild
-	 * @param guildId The ID of the guild
-	 * @param errorOnNull Should an error be logged if the document doesnt exist?
-	*/
-	export async function getDocument(guildId: string, errorOnNull: boolean = true) {
-		if (errorOnNull) {
-			try {
-				return Database.findOne({ _id: guildId });
-			} catch (e) {
-				const errMessage = `Attempted to find document of guild (${guildId})\n`;
-				if (e instanceof Error) {
-					e.message = errMessage + e.message
-					EmitError(e);
-				} else {
-					EmitError(new Error(errMessage));
-				}
-				return null;
-			}
-		}
-		else {
-			return Database.findOne({ _id: guildId });
-		}
-	}
-
 	/** Check if the mentionable is currently on cooldown
 	 * @param mentionable The mentionable object
 	 * @returns true if the mentionable is currently on cooldown, false otherwise
 	*/
-	export function isOncooldown(mentionable: IMentionableItem): boolean {
+	public static isOncooldown(mentionable: IMentionableItem): boolean {
 		return (mentionable.cooldown + mentionable.lastUsed >= Date.now())
 	}
 
@@ -75,7 +78,7 @@ export namespace Mentionable {
 	 * @param mentionable The mentionable object
 	 * @returns The remaining cooldown time
 	*/
-	export function remainingCooldown(mentionable: IMentionableItem): number {
+	public static remainingCooldown(mentionable: IMentionableItem): number {
 		return (mentionable.lastUsed + mentionable.cooldown) - Date.now()
 	}
 	//#endregion
@@ -86,26 +89,26 @@ export namespace Mentionable {
 	 * @param guildId The GuildID of the server
 	 * @returns The mentionable if found, null otherwise
 	*/
-	export async function create(guildId: string): Promise<typeof Database|unknown> {
-		hasChanged = true;
-		return await Database.create({_id: guildId}).catch(EmitError);
+	public static async create(guildId: string): Promise<typeof Database|unknown> {
+		Mentionable.hasChanged = true;
+		return await ObjectRelationalMap.create(Database, guildId);
 	}
 
 	/**  Update the mentionable document
 	 * @param doc The document to update
 	 * @returns Wether or not the data has been saved successfully
 	*/
-	export async function update(doc: Awaited<ReturnType<typeof getDocument>>): Promise<boolean>;
+	public static async update(doc: Awaited<ReturnType<typeof Mentionable.getDocument>>): Promise<boolean>;
 	/**  Update the mentionable document
 	 * @param guildId The GuildID of the server the document is for
 	 * @returns Wether or not the data has been saved successfully
 	*/
-	export async function update(guildId: string): Promise<boolean>;
-	export async function update(id_doc: string|Awaited<ReturnType<typeof getDocument>>): Promise<boolean> {
-		let doc: Awaited<ReturnType<typeof getDocument>>;
+	public static async update(guildId: string): Promise<boolean>;
+	public static async update(id_doc: string|Awaited<ReturnType<typeof Mentionable.getDocument>>): Promise<boolean> {
+		let doc: Awaited<ReturnType<typeof Mentionable.getDocument>>;
 
 		if (typeof id_doc === 'string') {
-			doc = await getDocument(id_doc);
+			doc = await Mentionable.getDocument(id_doc);
 			if (!doc) { return false; }
 		} else {
 			if (id_doc == null) { return false; }
@@ -115,7 +118,7 @@ export namespace Mentionable {
 		doc.markModified('mentionables');
 		await doc.save()
 
-		hasChanged = true;
+		Mentionable.hasChanged = true;
 		return true
 	}
 
@@ -125,12 +128,12 @@ export namespace Mentionable {
 	 * @param data The data of the mentionable to register
 	 * @returns Wether or not the data has been saved successfully
 	*/
-	export async function add(guildId: string, id: string, data: IMentionableItem): Promise<boolean> {
-		const doc = await getDocument(guildId);
+	public static async add(guildId: string, id: string, data: IMentionableItem): Promise<boolean> {
+		const doc = await Mentionable.getDocument(guildId);
 		if (!doc) { return false; }
 		
 		doc.mentionables[id] = data;
-		return await update(doc);
+		return await Mentionable.update(doc);
 	}
 
 	/** Edit a mentionable
@@ -139,8 +142,8 @@ export namespace Mentionable {
 	 * @param data The edited data
 	 * @returns Wether or not the data has been saved successfully
 	*/
-	export async function edit(guildId: string, id: string, data: IMentionableItem): Promise<boolean> {
-		return await add(guildId, id, data);
+	public static async edit(guildId: string, id: string, data: IMentionableItem): Promise<boolean> {
+		return await Mentionable.add(guildId, id, data);
 	}
 
 	/** Remove a mentionable
@@ -148,12 +151,12 @@ export namespace Mentionable {
 	 * @param id The ID of the mentionable
 	 * @returns Wether or not the data has been saved successfully
 	*/
-	export async function remove(guildId: string, id: string): Promise<boolean> {
-		const doc = await getDocument(guildId);
+	public static async remove(guildId: string, id: string): Promise<boolean> {
+		const doc = await Mentionable.getDocument(guildId);
 		if (!doc || !Object.keys(doc.mentionables).includes(id)) { return false; }
 
 		delete doc.mentionables[id]
-		return await update(doc);
+		return await Mentionable.update(doc);
 	}
 
 	/** Start the cooldown for a mentionable
@@ -162,7 +165,7 @@ export namespace Mentionable {
 	 * @param mentionable The mentionable object
 	 * @returns true if the cooldown has been started successfully, false otherwise
 	*/
-	export async function startCooldown(guild: Guild, id: string, mentionable?: IMentionableItem|null): Promise<boolean> {
+	public static async startCooldown(guild: Guild, id: string, mentionable?: IMentionableItem|null): Promise<boolean> {
 		const role = guild.roles.cache.find(r => r.id == id);
 		if (!role) {
 			EmitError(new Error(`Unable to find role (${id})`));
@@ -170,13 +173,13 @@ export namespace Mentionable {
 		}
 
 		if (mentionable === undefined) {
-			mentionable = await get(guild.id, id);
+			mentionable = await Mentionable.get(guild.id, id);
 		}
 		if (!mentionable) { return false; }
 
 		await role.setMentionable(false, `${process.env.APP_NAME} - Used`);
 		setTimeout( //TODO make an alternate system that doesnt use timeouts
-			async () => { onCooldownExpired(role); },
+			async () => { Mentionable.onCooldownExpired(role); },
 			mentionable.cooldown - (Date.now() - mentionable.lastUsed)
 		);
 
@@ -189,10 +192,10 @@ export namespace Mentionable {
 	/** Once the bot enters a new guild, see if we need to create a new document
 	 * @param guildId The GuildID of the server
 	*/
-	export async function onGuildCreate(guild: Guild): Promise<void> {
+	public static async onGuildCreate(guild: Guild): Promise<void> {
 		const doc = await Mentionable.getDocument(guild.id, false);
 		if (doc == null) {
-			await create(guild.id);
+			await Mentionable.create(guild.id);
 			cons.log(`[fg=green]Created[/>] new Mentionables document for ${guild.id}`);
 		}
 	}
@@ -200,7 +203,7 @@ export namespace Mentionable {
 	/** Once the bot leaves a guild, see if we need to delete a document
 	 * @param guildId The GuildID of the server
 	*/
-	export async function onGuildDelete(guild: Guild): Promise<void> {
+	public static async onGuildDelete(guild: Guild): Promise<void> {
 		const doc = await Mentionable.getDocument(guild.id, false);
 		if (doc != null) {
 			await doc.deleteOne({_id: doc._id})
@@ -213,13 +216,13 @@ export namespace Mentionable {
 	 * @param id The ID of the mentionable
 	 * @returns Wether or not the data has been saved successfully
 	*/
-	export async function onUsed(guild: Guild, id: string): Promise<boolean> {
-		const doc = await getDocument(guild.id);
+	public static async onUsed(guild: Guild, id: string): Promise<boolean> {
+		const doc = await Mentionable.getDocument(guild.id);
 		if (!doc || !doc.mentionables[id]) { return false; }
 
 		doc.mentionables[id].lastUsed = new Date().getTime();
-		await startCooldown(guild, id, doc.mentionables[id]);
-		return await update(doc);
+		await Mentionable.startCooldown(guild, id, doc.mentionables[id]);
+		return await Mentionable.update(doc);
 	}
 
 	//TODO fix wording on this summary
@@ -227,7 +230,7 @@ export namespace Mentionable {
 	 * @param role The role of the mentionable that the cooldown expired of
 	 * @returns Wether or not the role has been set to mentionable successfully
 	*/
-	export async function onCooldownExpired(role: Role): Promise<boolean> {
+	public static async onCooldownExpired(role: Role): Promise<boolean> {
 		await role.setMentionable(true, `${process.env.APP_NAME} - Cooldown Expired`).catch((e: Error) => {
 			e.message = `Failed to update role to mentionable after expired cooldown\n${e.message}`
 			EmitError(e); //! if this is reached, there is a role stuck on not mentionable
