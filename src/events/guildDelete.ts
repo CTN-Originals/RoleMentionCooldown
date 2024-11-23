52809824dd9b3ac95203271a2c11057f1ad45d25
CTN <info@ctn-originals.com>
2024-11-23 23:16:16 +0100

    Delete mentionable cache onGuildDelete
    The cache would have just stayed in there as long as the bot didnt restart... so that might have been bad if this bot takes of in any way.

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -12,6 +12,8 @@ export default {
 		eventConsole.log(`\nOn: [fg=green]${this.name}[/>]\nName: ${guild.name}\nID: ${guild.id}\nMembers: ${guild.memberCount}\n`)
 		
 		await Mentionable.onGuildDelete(guild);
+		delete Mentionable.mentionablesCache[guild.id]; //? Delete the guilds cache
+		
 		await GuildConfig.onGuildDelete(guild);
 	},
 }
