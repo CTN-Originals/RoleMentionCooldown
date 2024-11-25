8d261746d5f42f403c676090a45d59e94fb65cf7
Damon Blais <damon.blais@gmail.com>
2024-11-25 10:04:06 +0100

    fix(lint): Proper spacing, comment order for types

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -1,12 +1,21 @@
 import {
+	ChannelSelectMenuInteraction,
 	Collection,
-	StringSelectMenuInteraction, ChannelSelectMenuInteraction, UserSelectMenuInteraction, MentionableSelectMenuInteraction, RoleSelectMenuInteraction,
-} from "discord.js";
+	MentionableSelectMenuInteraction,
+	RoleSelectMenuInteraction,
+	StringSelectMenuInteraction,
+	UserSelectMenuInteraction,
+} from "discord.js"
 
 declare module "discord.js" {
 	export interface Client {
-		commands: Collection<any, any>;
+		commands: Collection<any, any>
 	}
 }
 
-export type AnyComponentInteraction = StringSelectMenuInteraction|ChannelSelectMenuInteraction|UserSelectMenuInteraction|MentionableSelectMenuInteraction|RoleSelectMenuInteraction
+export type AnyComponentInteraction =
+	StringSelectMenuInteraction |
+	ChannelSelectMenuInteraction |
+	UserSelectMenuInteraction |
+	MentionableSelectMenuInteraction |
+	RoleSelectMenuInteraction
