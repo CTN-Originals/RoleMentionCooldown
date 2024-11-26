780574842a18d23e3c5bfca6a12e714e2e22a15f
CTN <info@ctn-originals.com>
2024-11-26 14:41:14 +0100

    Update ping.ts
    idk what was happening here...

--- src/commands/test/ping.ts
+++ src/commands/test/ping.ts
@@ -9,7 +9,7 @@ export default {
 			.setName("ping")
 			.setDescription("Replies with latency stats"),
 		async execute(interaction: ChatInputCommandInteraction) {
-			const commandPing = (GeneralData.development) ? interaction.createdTimestamp - Date.now() : Date.now() - interaction.createdTimestamp;
+			const commandPing = Date.now() - interaction.createdTimestamp;
 			const apiPing = interaction.client.ws.ping;
 
 			await interaction.reply({
