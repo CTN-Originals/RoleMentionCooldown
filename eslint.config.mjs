ce90fae38ee21ffe07e965e5617faa97e436f3b1
CTN <info@ctn-originals.com>
2025-02-13 14:44:39 +0100

    Added eslint
    Added linter and set it up with a basic boiler plate from Damon.

---
+++ eslint.config.mjs
@@ -0,0 +1,68 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import eslint from '@eslint/js'
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import eslintImportPlugin from 'eslint-plugin-import'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import tslint from 'typescript-eslint'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: eslint.configs.recommended,
+	allConfig: eslint.configs.all
+})
+
+const config = [
+	{
+		ignores: [
+			// ...
+		],
+	},
+	eslint.configs.recommended,
+	tslint.configs.eslintRecommended,
+	...tslint.configs.recommended,
+	...compat.extends(''),
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.mts'],
+		languageOptions: {
+			ecmaVersion: 5,
+			parser: tsParser,
+			parserOptions: {
+				project: './tsconfig.json'
+			},
+			sourceType: 'script'
+		},
+		plugins: {
+			'import': eslintImportPlugin,
+			'@typescript-eslint': tsEslintPlugin,
+		},
+		rules: {
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'@typescript-eslint/explicit-function-return-type': ['warn', {
+				allowExpressions: true
+			}],
+			'import/prefer-default-export': 'off',
+			indent: ['error', 2],
+			'key-spacing': ['error', {
+				multiLine: {
+					beforeColon: false,
+					afterColon: true
+				},
+				
+				align: {
+					beforeColon: false,
+					afterColon: true,
+					on: 'value'
+				}
+			}],
+			'linebreak-style': ['error', 'unix'],
+			quotes: ['error', 'single'],
+			semi: ['error', 'never']
+		}
+	}
+]
+
+export default config
