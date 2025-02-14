bc0dafea7d968aaa9b5f9d914aba8d2da3bca13e
CTN <info@ctn-originals.com>
2025-02-14 17:35:46 +0100

    Finished the componentValueStorage class #51

--- src/handlers/componentValueStorage.ts
+++ src/handlers/componentValueStorage.ts
@@ -6,21 +6,29 @@ type ComponentValueHolder = {[componentId: string]: ComponentValue | null};
 export class ComponentValueStorage {
 	private static storage: {[messageId: string]: ComponentValueHolder} = {};
 
-	/** Start the timer anc cleanup the message ID from the storage once it runs out
-	 * @note Discords interaction have a hardset lifetime of 15 minutes and will be cleared out from memory once that time is up,
-		so there is not reason for us to keep this data around after that time either as the components on that message will no longer call back to us with interactions
-	 * @note By the time this function is called, the message likely already existed for a little bit 
-		because a message ID is only registered once a user interacts with a component that is on a message that has not been registered yet, 
-		instead of it being created once the reply is sent (which makes more sense but is harder to do)
-	 * @param messageId The message ID key in the storage object to delete once the timer runs out
+
+	/** Get an item out of the storage object by its message id
+	 * @param messageId The message id to check if it is included
+	 * @returns `true` if the storage contains the message ID as a key, `false` otherwise
 	*/
-	private static async setCleanupTimeout(messageId: string) {
-		new Promise<void>((resolve) => {
-			setTimeout(() => {
-				delete ComponentValueStorage.storage[messageId];
-				resolve();
-			}, 1000 * 60 * 15);
-		});
+	public static storageIncludesMessage(messageId: string): boolean {
+		return Object.keys(ComponentValueStorage.storage).includes(messageId);
+	}
+
+	/** Register a new message ID to the storage object
+	 * @param messageId The ID of the message you want to register
+	 * @returns `true` if the message ID was added to the storage object, `false` if the ID was already present in the storage object
+	*/
+	public static registerMessage(messageId: string): boolean {
+		if (ComponentValueStorage.storageIncludesMessage(messageId)) {
+			EmitError(new Error(`Message ID was already present in storage (${messageId})`));
+			return false;
+		}
+
+		ComponentValueStorage.storage[messageId] = {};
+		ComponentValueStorage.setCleanupTimeout(messageId);
+
+		return true;
 	}
 
 	/** Register a new interaction that has just been replied to
@@ -38,31 +46,7 @@ export class ComponentValueStorage {
 			return false;
 		}
 
-		ComponentValueStorage.storage[message.id] = {};
-		ComponentValueStorage.setCleanupTimeout(message.id);
-
-		return true;
-	}
-
-	/** Get an item out of the storage object by its message id
-	 * @param messageId The message id to check if it is included
-	 * @returns `true` if the storage contains the message ID as a key, `false` otherwise
-	*/
-	public static storageIncludesMessage(messageId: string): boolean {
-		return Object.keys(ComponentValueStorage.storage).includes(messageId);
-	}
-
-	/** Get an item out of the storage object by its message id
-	 * @param messageId The message id of which to get the item
-	 * @returns The {@linkcode ComponentValueHolder} if it exists or `false` otherwise
-	*/
-	private static getItemByMessageId(messageId: string): ComponentValueHolder | false {
-		if (!ComponentValueStorage.storageIncludesMessage(messageId)) {
-			EmitError(new Error(`Component value storage does not contain message item (${messageId})`));
-			return false;
-		}
-
-		return ComponentValueStorage.storage[messageId];
+		return ComponentValueStorage.registerMessage(message.id);
 	}
 
 	public static setValue(messageId: string, componentId: string, value: ComponentValue): boolean {
@@ -79,8 +63,34 @@ export class ComponentValueStorage {
 		const storageItem = ComponentValueStorage.getItemByMessageId(messageId);
 		if (!storageItem) { return null; }
 
+		return (Object.keys(storageItem).includes(componentId)) ? storageItem[componentId] : null;
+	}
 
 
-		return null;
+	/** Start the timer anc cleanup the message ID from the storage once it runs out
+	 * @note Discords interaction have a hardset lifetime of 15 minutes and will be cleared out from memory once that time is up,
+		so there is not reason for us to keep this data around after that time either as the components on that message will no longer call back to us with interactions
+	 * @note By the time this function is called, the message likely already existed for a little bit 
+		because a message ID is only registered once a user interacts with a component that is on a message that has not been registered yet, 
+		instead of it being created once the reply is sent (which makes more sense but is harder to do)
+	 * @param messageId The message ID key in the storage object to delete once the timer runs out
+	*/
+	private static async setCleanupTimeout(messageId: string): Promise<void> {
+		setTimeout(() => {
+			delete ComponentValueStorage.storage[messageId];
+		}, 1000 * 60 * 15);
+	}
+
+	/** Get an item out of the storage object by its message id
+	 * @param messageId The message id of which to get the item
+	 * @returns The {@linkcode ComponentValueHolder} if it exists or `false` otherwise
+	*/
+	private static getItemByMessageId(messageId: string): ComponentValueHolder | false {
+		if (!ComponentValueStorage.storageIncludesMessage(messageId)) {
+			EmitError(new Error(`Component value storage does not contain message item (${messageId})`));
+			return false;
+		}
+
+		return ComponentValueStorage.storage[messageId];
 	}
 }
